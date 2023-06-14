84158f421728dff865a1fed7751cbced36414bd3
Jorge L. Rdguez <jluisaiza80@gmail.com>
2023-06-13 21:50:48 -0400

    Subiendo mi proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+
+const buttonIniciar = document.getElementById('buttonIniciar');
+const button__agregar = document.getElementById('button__agregar');
+
+buttonIniciar.addEventListener('click', () => {
+    window.location.href = "game.html";
+});
+
+button__agregar.addEventListener('click', () => {
+    window.location.href = 'palabras.html'
+})
